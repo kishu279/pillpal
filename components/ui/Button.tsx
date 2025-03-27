b182983d11f03173f26062862eea15bf32faef70
ankit <sahuankit.murhu@gmail.com>
2025-03-27 16:51:38 +0530

    Button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,11 +1,14 @@
+import colors from "@/styles/colors";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export default function Button() {
+export default function Button({ value }: { value: string }) {
   return (
     <TouchableOpacity
       style={styles.buttonText}
     >
-      <Text>Lmao</Text>
+      <Text
+        style={styles.buttonTextLabel}
+      >{value}</Text>
     </TouchableOpacity>
   )
 }
@@ -13,8 +16,21 @@ export default function Button() {
 
 const styles = StyleSheet.create({
   buttonText: {
-    backgroundColor: 'grey',
-    padding: 10,
-    borderRadius: 5,
-  }
-})
+    backgroundColor: colors["zinc-200"],
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  buttonTextLabel: {
+    color: colors["zinc-900"],
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
